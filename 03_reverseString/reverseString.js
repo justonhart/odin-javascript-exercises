3b6d202572839b8a3d081e6ef32a3006943d25e1
Juston Hart <contact@justonh.art>
2024-01-23 10:07:34 -0600

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	let returnVal = "";
+	for(let i = 0; i < string.length; i++){
+		returnVal += string[string.length - 1 - i];
+	}
+	return returnVal;
 };
 
 // Do not edit below this line
