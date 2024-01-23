a7dd444f50abbd01e5a2f129567b78fa9dfcd09c
Juston Hart <contact@justonh.art>
2024-01-23 10:17:29 -0600

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valuesToRemove) {
+	let returnArr = [];
 
+	arr.forEach(val => {
+		if(!valuesToRemove.includes(val)){
+			returnArr.push(val);
+		}
+	});
+	return returnArr;
 };
 
 // Do not edit below this line
