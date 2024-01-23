7e8a17aabd0c741a6d23f7f4ea1735249e7370c4
Juston Hart <contact@justonh.art>
2024-01-23 10:37:41 -0600

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+// Note, I've written this recursively as a challenge to myself to remember how to do recursion, not because it's necessarily a good idea
+const sumAll = function(start, end) {
+	if(typeof start != "number" || typeof end != "number" || start < 0 || end < 0){
+		return "ERROR"
+	}
 
+	function recurse(start, endCase){
+		if(start === endCase){
+			return start;
+		} 
+		return start + recurse(start + 1, endCase);
+	}
+
+	if(start > end){
+		return recurse(end, start);
+	}
+
+	return recurse(start, end);
 };
 
 // Do not edit below this line
