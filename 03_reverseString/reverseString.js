7a69544d6cd32521373bcf28c3286a7bcf88a16b
Juston Hart <contact@justonh.art>
2024-01-23 10:21:35 -0600

    Rewrite 03 solution to use array functions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,5 @@
 const reverseString = function(string) {
-	let returnVal = "";
-	for(let i = 0; i < string.length; i++){
-		returnVal += string[string.length - 1 - i];
-	}
-	return returnVal;
+	return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
