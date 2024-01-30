8b27b0e9504ebda4fc630d7f453545e2da5fdc12
Juston Hart <contact@justonh.art>
2024-01-30 13:24:12 -0600

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+	if( x < 0 ){
+		return "OOPS";
+	}
+	if(x == 0){
+		return 0;
+	}
+	if(x == 1 || x == 2){
+		return 1;
+	}
 
+	return fibonacci(x - 1) + fibonacci(x - 2);
 };
 
 // Do not edit below this line
