a079b3cdedba37e98160e97b269b65c7e91290ad
Juston Hart <contact@justonh.art>
2024-01-30 09:56:20 -0600

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(...args) {
+	return args.reduce((sum, next) => sum + next, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	return args.reduce((diff, next) => diff - next);
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	return args.reduce((sum, next) => sum + next, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+	return args.reduce((result, next) => result * next);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if(x === 0 || x === 1){
+		return 1;
+	}
+
+	return x * factorial(x-1);
 };
 
 // Do not edit below this line
