5e37896361b9d580aab540ceca2eb13987069d8f
Juston Hart <contact@justonh.art>
2024-01-30 14:22:54 -0600

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+	const currentYear = new Date().getFullYear();
+	return arr.reduce((oldest, next) => {
+		if(!oldest.age){
+			oldest.age = (oldest.yearOfDeath ?? currentYear ) - oldest.yearOfBirth;
+		}
+		if(!next.age){
+			next.age = (next.yearOfDeath ?? currentYear ) - next.yearOfBirth;
+		}
 
+		return oldest.age > next.age ? oldest : next;
+	});	
 };
 
 // Do not edit below this line
