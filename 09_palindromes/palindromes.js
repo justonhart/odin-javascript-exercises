b859a8c723f25f2b90746c478f2fea71e7b9b00f
Juston Hart <contact@justonh.art>
2024-01-30 10:21:02 -0600

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+/**
+ * @param {string} string
+ */
+const palindromes = function (string) {
+	//remove puntuation
+	const cleanedString = string.replaceAll(',', '').replaceAll('.', '').replaceAll('!', '').replaceAll(' ', '').toLowerCase();
+	console.log(cleanedString);
+	let palindrome = true;
+	for(let i = 0; i < cleanedString.length / 2; i++){
+		if(cleanedString[i] != cleanedString[(cleanedString.length - i) - 1]){
+			palindrome = false;
+			i = cleanedString.length;
+		}
+	}
 
+	return palindrome;
+	
 };
 
 // Do not edit below this line
